9c129a11da51a2c84601b1ce1657a2d160b952b3
Thomas Shaw <tomwillfixit@users.noreply.github.com>
2016-05-12 21:42:55 +0000

    Just for reference

---
+++ import_empty_db.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+#purely for reference purposes.  I ran each of the db commands manually to get this to work.
+
+service mysql stop
+
+mysqld_safe --skip-grant-tables &
+
+sleep 5
+
+function log() {
+  echo "[+] $1"
+}
+
+function import_empty_db() {
+  local __u="ctf"
+  local __p="ctf"
+  local __user=$1
+  local __pwd=$2
+  local __db=$3
+  local __path=$4
+  local __mode=$5
+
+  echo $__path
+
+  log "Creating DB - $__db"
+  mysql -u "$__user" --password="$__pwd" -e "CREATE DATABASE \`$__db\`;"
+
+  log "Importing schema..."
+  mysql -u "$__user" --password="$__pwd" "$__db" -e "source $__path/database/schema.sql;"
+  log "Importing countries..."
+  mysql -u "$__user" --password="$__pwd" "$__db" -e "source $__path/database/countries.sql;"
+  log "Importing logos..."
+  mysql -u "$__user" --password="$__pwd" "$__db" -e "source $__path/database/logos.sql;"
+
+  log "Creating user..."
+  mysql -u "$__user" --password="$__pwd" -e "CREATE USER '$__u'@'localhost' IDENTIFIED BY '$__p';"
+  mysql -u "$__user" --password="$__pwd" -e "GRANT ALL PRIVILEGES ON \`$__db\`.* TO '$__u'@'localhost';"
+  mysql -u "$__user" --password="$__pwd" -e "FLUSH PRIVILEGES;"
+
+  log "DB Connection file"
+  cat "$__path/extra/settings.ini.example" | sed "s/DATABASE/$__db/g" | sed "s/MYUSER/$__u/g" | sed "s/MYPWD/$__p/g" > "$__path/settings.ini"
+
+  local PASSWORD
+  log "Adding default admin user"
+  if [[ $__mode = "dev" ]]; then
+    PASSWORD='password'
+  else
+    PASSWORD=$(head -c 500 /dev/urandom | md5sum | cut -d" " -f1)
+  fi
+
+  log "The password for admin is: $PASSWORD"
+  HASH=$(hhvm -f "$__path/extra/hash.php" "$PASSWORD")
+  mysql -u "$__user" --password="$__pwd" "$__db" -e "INSERT INTO teams (name, password_hash, admin, protected, logo, created_ts) VALUES('admin', '$HASH', 1, 1, 'admin', NOW());"
+}
+
+import_empty_db
